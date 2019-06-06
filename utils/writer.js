bcf20db00a1bca7d6eb168c14943f04cab9d6d56
carmine-alberto <48260691+carmine-alberto@users.noreply.github.com>
2019-06-06 16:56:57 +0200

    Set new header
    Set new header to prevent CORS warnings:  "Access-Control-Allow-Origin: *"

--- utils/writer.js
+++ utils/writer.js
@@ -1,5 +1,6 @@
 
 module.exports = function (context, code, payload) {
     context.res.statusCode = code;
+    context.res.setHeader("Access-Control-Allow-Origin", "*");
     context.res.setBody(JSON.stringify(payload, null, 2));
 }
