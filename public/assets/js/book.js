25cd8fe9c98e9900a901382cb7a32b4632d1c8a7
Giuseppe Italia <48276031+beppe22@users.noreply.github.com>
2019-07-03 15:21:51 +0200

    Update book.js

--- public/assets/js/book.js
+++ public/assets/js/book.js
@@ -261,37 +261,12 @@ const appendDataforOtherBook= function(data)
 }
 
 
-const appendDataForListThemes= function(selector, list)
-{
-    for(var i=0;i<list.length;i++)
-        {
-            selector.append(
-                '<li id="theme_'+list[i]+'" role="option"><a href="https://bookaholic.herokuapp.com/theme?theme='+list[i]+'">'+list[i]+'</a></li>'
-                )
-
-        }
-
-}
-
-const appendDataForListGenres= function(selector, list)
-{
-    for(var k=0; k<list.length; k++)
-        {
-            selector.append(
-               ' <li id="genre_'+list[k]+'"  role="option"> <a href="https://bookaholic.herokuapp.com/genre?genre='+list[k]+'">'+list[k]+'</a></li>'
-            )
-        }
-
-}
-
 
 
 
 
 //MAIN
 
-var genre_list= $("#genre_list");
-var themes_list= $("#themes_list");
 
  //Get id of author from URL
 var Url = self.location.href;
@@ -336,14 +311,4 @@ fetch(host + "/api/books/"+id)
         .then(events => appendDataforEvents(related_events, events))
 
         )
-.then(
-    fetch(host+"/api/books/themes")
-    .then(response => response.json())
-    .then(themes=> appendDataForListThemes(themes_list, themes))
-)
-.then(
 
-    fetch(host+"/api/books/genres")
-    .then(response => response.json())
-    .then(genres=> appendDataForListGenres(genre_list, genres))
-)
