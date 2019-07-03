15f6c34abbcf68bf4ef8ee33f1159f59dc92548a
Giuseppe Italia <48276031+beppe22@users.noreply.github.com>
2019-07-03 23:21:31 +0200

    Update single_event1.js

--- public/assets/js/single_event1.js
+++ public/assets/js/single_event1.js
@@ -38,28 +38,7 @@ const addDataforEvent= function(selector1, data, selector2)
         )
 }
 
-const appendDataForListThemes= function(selector, list)
-{
-    for(var i=0;i<list.length;i++)
-        {
-            selector.append(
-                '<li id="theme_'+list[i]+'" role="option"><a href="https://bookaholic.herokuapp.com/theme?theme='+list[i]+'">'+list[i]+'</a></li>'
-                )
-                
-        }
-    
-}
 
-const appendDataForListGenres= function(selector, list)
-{
-    for(var k=0; k<list.length; k++)
-        {
-            selector.append(
-               ' <li id="genre_'+list[k]+'"  role="option"> <a href="https://bookaholic.herokuapp.com/genre?genre='+list[k]+'">'+list[k]+'</a></li>'
-            )
-        }
-    
-}
 
 
 
@@ -67,8 +46,7 @@ const appendDataForListGenres= function(selector, list)
 
 //MAIN
 
-var genre_list= $("#genre_list");
-var themes_list= $("#themes_list");
+
 
 var event_class= $(".event");
 var book_details= $("#book_details");
@@ -81,15 +59,5 @@ var id= getUrlParameter(Url).event_id;
 fetch(host + "/api/events/"+id)
 .then(response => response.json())
 .then(event=> addDataforEvent(event_class, event,book_details))
-.then(
-    fetch(host+"/api/books/themes")
-    .then(response => response.json())
-    .then(themes=> appendDataForListThemes(themes_list, themes))
-)
-.then(
 
-    fetch(host+"/api/books/genres")
-    .then(response => response.json())
-    .then(genres=> appendDataForListGenres(genre_list, genres))
-)
 
