9f9869bb4c5d76d0afa55ee1b67bcc39e4c5d8f2
carmine-alberto <carmine.alberto@mail.polimi.it>
2025-06-05 05:35:38 +0200

    Update website URL

--- service/DataLayer.js
+++ service/DataLayer.js
@@ -1,11 +1,11 @@
-console.log("diocane però qua entra");
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = (require("knex"))({
     client: 'pg',
     connection: {
-      connectionString: "postgres://i:SEDI9QyhkNGN2pi7a8nloU7UageDv4O4@dpg-d0ufvfmmcj7s739l78dg-a.frankfurt-postgres.render.com:5432/bh_db_xc3m",
-      ssl: { rejectUnauthorized: false }
+      connectionString: process.env.DATABASE_URL || "postgres://i:SEDI9QyhkNGN2pi7a8nloU7UageDv4O4@dpg-d0ufvfmmcj7s739l78dg-a.frankfurt-postgres.render.com:5432/bh_db_xc3m",
+      ssl: !isProduction ? { rejectUnauthorized: false } : false
     }
 });
 
-console.log("diocane però questo l'esporta")
+
